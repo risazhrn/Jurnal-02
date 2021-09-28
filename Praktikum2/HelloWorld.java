6ba4c8dec822227d2a4456d5e21a762795afd9c9
risazhrn <risazahrani@student.telkomuniversity.ac.id>
2021-09-28 12:55:10 +0700

    HelloWorld.java

---
+++ Praktikum2/HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld {
+	
+	public static void main(String[] args) {
+		System.out.println("Nama : Risa Zahrani");
+		System.out.println("NIM : 6706213100");
+		System.out.println("Kelas : D3IF-45-01");
+	}
+}
